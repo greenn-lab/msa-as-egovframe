5a7b997b73ebb2f62ff1b5ebfe4b131ffbc75cce
greenn <greenn.lab@gmail.com>
2021-10-28 15:56:30 +0900

    update// Circuit Breaker Pattern 적용

--- catalogs/src/main/java/io/github/greennlab/msa/catalogs/CatalogsApplication.java
+++ catalogs/src/main/java/io/github/greennlab/msa/catalogs/CatalogsApplication.java
@@ -2,9 +2,18 @@ package io.github.greennlab.msa.catalogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class CatalogsApplication {
+
+  @Bean
+  RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(CatalogsApplication.class, args);
--- catalogs/src/main/java/io/github/greennlab/msa/catalogs/service/CustomerApiServiceImpl.java
+++ catalogs/src/main/java/io/github/greennlab/msa/catalogs/service/CustomerApiServiceImpl.java
@@ -1,5 +1,6 @@
 package io.github.greennlab.msa.catalogs.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,11 +11,24 @@ public class CustomerApiServiceImpl implements CustomerApiService {
 
   private final RestTemplate restTemplate;
 
+  @HystrixCommand(fallbackMethod = "getCustomerDetailFallback")
   @Override
   public String getCustomerDetail(String customerId) {
     final String url = "http://localhost:8082/customers/" + customerId;
 
+    //noinspection ConstantConditions
+    if (true) {
+      throw new RuntimeException("Network down");
+    }
+
     return restTemplate.getForObject(url, String.class);
+  }
+
+  @SuppressWarnings("unused")
+  private String getCustomerDetailFallback(String customerId, Throwable thrown) {
+    return String.format("Sorry for too late loading %s information: %s",
+        customerId,
+        thrown.getMessage());
   }
 
 }
