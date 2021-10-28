f02ead33fbf46d84c7623ea0802be26089776748
greenn <greenn.lab@gmail.com>
2021-10-28 15:48:14 +0900

    update//customer 서비스 호출

--- catalogs/src/main/java/io/github/greennlab/msa/catalogs/service/CustomerApiServiceImpl.java
+++ catalogs/src/main/java/io/github/greennlab/msa/catalogs/service/CustomerApiServiceImpl.java
@@ -2,14 +2,19 @@ package io.github.greennlab.msa.catalogs.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerApiServiceImpl implements CustomerApiService {
 
+  private final RestTemplate restTemplate;
+
   @Override
   public String getCustomerDetail(String customerId) {
-    return customerId;
+    final String url = "http://localhost:8082/customers/" + customerId;
+
+    return restTemplate.getForObject(url, String.class);
   }
 
 }
