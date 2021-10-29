174dd6352113c36967c35b1e71a4184c6ad7a004
greenn <greenn.lab@gmail.com>
2021-10-29 09:57:34 +0900

    update// service registry 추가

---
+++ service-registry-eureka/src/test/java/io/github/greennlab/mas/serviceregistryeureka/ServiceRegistryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.greennlab.mas.serviceregistryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryEurekaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
